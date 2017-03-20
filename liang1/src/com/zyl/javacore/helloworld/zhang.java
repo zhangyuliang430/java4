915e2a2c0f5f923d155f02e3f2bc417d9a5e6f4c
liang <2256767903@qq.com>
2017-03-20 18:40:05 +0800

    this ib moeny Signed-off-by: liang <2256767903@qq.com>

---
+++ liang1/src/com/zyl/javacore/helloworld/zhang.java
@@ -0,0 +1,15 @@
+package com.zyl.javacore.helloworld;
+
+public class zhang {
+
+	public static void main(String[] args) {
+		int yu = 25;
+		//定义变量
+		System.out.println("今年准备赚多少个亿：" + yu);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
