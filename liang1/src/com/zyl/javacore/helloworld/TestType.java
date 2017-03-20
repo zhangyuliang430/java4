c06f80451642dbf949f52ae01a001af3b6a754da
liang <2256767903@qq.com>
2017-03-20 18:20:44 +0800

    this is my first commit Sig
    Signed-off-by: liang <2256767903@qq.com>

---
+++ liang1/src/com/zyl/javacore/helloworld/TestType.java
@@ -0,0 +1,14 @@
+package com.zyl.javacore.helloworld;
+
+public class TestType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ double score = 99.6;
+ String name = "金三胖";
+ char  sex = '男';
+ System.out.println("成绩最屌的分数是：" + score);		 
+ System.out.println("成绩最屌的姓名：" + name);	
+ System.out.println("成绩最屌的性别是：" + sex);		}
+
+}
